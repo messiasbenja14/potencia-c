9dff9d15cfe216890db8b9e4f9d62b9b9fd2baf6
Messias <messias.benja14@gmail.com>
2026-07-29 14:19:27 -0300

    Update function_potencia.c

--- function_potencia.c
+++ function_potencia.c
@@ -1,1 +1,7 @@
-
+double potencia(double base, double potencia) {
+    double resultado = 1;
+    for (int i = 0; i < potencia; i++) {
+        resultado = resultado * base;
+    }
+    return resultado;
+}
