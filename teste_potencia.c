58d88a5b86c032e72bc8e8bda4c6f91b683a45f9
Messias <messias.benja14@gmail.com>
2026-07-29 14:23:36 -0300

    Update and rename test_potencia.c to teste_potencia.c

--- teste_potencia.c
+++ teste_potencia.c
@@ -1,6 +1,6 @@
-#include "function_potencia.c"
 #include <stdio.h>
 #include <assert.h>
+#include "function_potencia.c"
 
 int main(int argc, char const *argv[]) {
     assert(potencia(2, 2) == 4 && potencia(3, 3) == 27 && potencia(4, 4) == 256 && potencia(5, 5) == 3125);
