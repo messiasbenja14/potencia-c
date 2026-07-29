a13aeb497ca495db07e801ea7ac364494c5fa899
Messias <messias.benja14@gmail.com>
2026-07-29 14:34:42 -0300

    Update testes.c

--- testes.c
+++ testes.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
-#include <assert.h>
-#include "function_potencia.h"
+#include <stdlib.h>
+#include "function_potencia.c"
 
 int main(int argc, char const *argv[]) {
-    assert(potencia(2, 2) == 4 && potencia(3, 3) == 27 && potencia(4, 4) == 256 && potencia(5, 5) == 3125);
-    printf("Teste concluido com sucesso\n");
+    double num1, num2;
+
+    if (argc >= 3) {
+        num1 = atof(argv[1]);
+        num2 = atof(argv[2]);
+    } else {
+        num1 = 5;
+        num2 = 2;
+    }
+
+    double res = potencia(num1, num2);
+
+    printf("resultado: %f\n", res);
+    printf("\nFeito por benjamim messias\n");
+
+    FILE *arquivo = fopen("potencia.txt", "w");
+    if (arquivo != NULL) {
+        fprintf(arquivo, "resultado: %f\n", res);
+        fprintf(arquivo, "\nFeito por benjamim messias\n");
+        fclose(arquivo);
+    }
 
     return 0;
 }
