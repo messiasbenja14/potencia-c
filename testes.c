0b8376a735c10e80ad1d43d5d6b85958a3092588
Messias <messias.benja14@gmail.com>
2026-07-29 14:30:18 -0300

    Update testes.c

--- testes.c
+++ testes.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
-#include "function_potencia.c"
+#include "function_potencia.h"
 
 int main(int argc, char const *argv[]) {
     assert(potencia(2, 2) == 4 && potencia(3, 3) == 27 && potencia(4, 4) == 256 && potencia(5, 5) == 3125);
