7c05a63423062395a4350c1d7b5d72e59d99ad0c
Messias <messias.benja14@gmail.com>
2026-07-29 14:19:52 -0300

    Update test_potencia.c

--- test_potencia.c
+++ test_potencia.c
@@ -1,1 +1,10 @@
+#include "function_potencia.c"
+#include <stdio.h>
+#include <assert.h>
 
+int main(int argc, char const *argv[]) {
+    assert(potencia(2, 2) == 4 && potencia(3, 3) == 27 && potencia(4, 4) == 256 && potencia(5, 5) == 3125);
+    printf("Teste concluido com sucesso\n");
+
+    return 0;
+}
